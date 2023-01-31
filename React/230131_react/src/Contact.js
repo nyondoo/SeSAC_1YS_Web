e59c0315bd714609751e6cf8a4aa5540493ddc9b
nyondoo <ahfl1129@gmail.com>
2023-01-31 17:50:48 +0900

    feat: 리액트 리덕스 강의

---
+++ React/230131_react/src/Contact.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+export default function Contact() {
+  const list = useSelector((state) => state);
+  const dispatch = useDispatch();
+  function writeList() {
+    let action = {
+      type: 'ADD',
+      payload: {
+        writer: '1',
+        title: 'a',
+      },
+    };
+    dispatch(action);
+  }
+  return (
+    <div>
+      <form id="form_write" style={{ marginBottom: '15px' }}>
+        <input type="text" name="writer" placeholder="작성자" />
+        <input type="text" name="title" placeholder="제목" />
+        <button type="button" onClick={writeList}>
+          작성
+        </button>
+      </form>
+
+      <table border={1}>
+        <thead>
+          <tr>
+            <th>번호</th>
+            <th>제목</th>
+            <th>작성자</th>
+          </tr>
+        </thead>
+        <tbody>
+          {list.map((value, i) => {
+            // value = { writer: ~~ , title~~ };
+            return (
+              <tr>
+                <td>{i + 1}</td>
+                <td>{value.title}</td>
+                <td>{value.writer}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}

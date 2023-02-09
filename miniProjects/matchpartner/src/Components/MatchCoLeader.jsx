cdc669c41dfdfc8eece76115247b71be511cde3e
nyondoo <ahfl1129@gmail.com>
2023-02-09 09:39:55 +0900

    다시

--- miniProjects/matchpartner/src/Components/MatchCoLeader.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function MatchCoLeader(props) {
-    <span>{props.name}</span>
-}
--- miniProjects/matchpartner/src/Components/MatchLeader.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function MatchLeader(props) {
-    <span>{props.name}</span>
-}
